746b8204504940a7ba6f89afcaa2fd2285fdb559
ZINEB LACHGHAR <103351409+zineblachghar199@users.noreply.github.com>
2024-06-10 13:26:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ module "msk_cluster" {
   source = "./MSK"
 
   #aws_region          = "us-east-1"
-  cluster_name        = "my-msk-cluster"
+  cluster_name        = "mansa-msk-cluster-interns"
   #kafka_version       = "2.8.0"
   #number_of_broker_nodes = 2
   #broker_instance_type = "kafka.m5.large"
   #ebs_volume_size     = 100
-  subnet_ids          = ["subnet-040289f3533a4d4e2", "subnet-0864c77375bd4a6fa"] 
-  security_group_ids  = ["sg-0af507fd11ad464dd"]
+  subnet_ids          = ["subnet-0b36c6c08b5baf9c2", "subnet-0c56e5bc834623aed"] 
+  security_group_ids  = ["sg-03da91baf42537dae"]
   tags                = {
     Environment = "dev1"
-    Project     = "msk-project1"
+    Project     = "mansa-msk-project1"
   }
 }
